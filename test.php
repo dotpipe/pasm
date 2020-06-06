be0187f78c42a122fb21373441b392d18825ddf5
Caleb Mazalevskis <maikuolan@gmail.com>
2020-06-06 17:34:09 +0800

    Normalise indenting and treat include_once as statement.
    ..instead of as a function (both work fine, but it looks nicer this way).
    
    :-)

--- test.php
+++ test.php
@@ -1,29 +1,28 @@
 <?php
+include_once 'pasm.php';
 
-include_once('pasm.php');
+$x = new PASM();
 
-    $x = new PASM();
+$x->set('ecx',3)    // REGISTER
+    ->set('ldp',2)  // NUMBER OF COMMANDS TO GO BACK
+    ->set('pdb',1)  // DEBUG FIELD
+    ->set('rdx',3)  // REGISTER
+    ->set('ah',2)   // REGISTER
+    ->end();
 
-    $x->set('ecx',3)    // REGISTER
-        ->set('ldp',2)  // NUMBER OF COMMANDS TO GO BACK
-        ->set('pdb',1)  // DEBUG FIELD
-        ->set('rdx',3)  // REGISTER
-        ->set('ah',2)   // REGISTER
-        ->end();
+$y = 'ecx';
+//print_r($x);
+$x->mov_ecx()->decr()->jne()->loope()->end();
 
-    $y = "ecx";
-    //print_r($x);
-    $x->mov_ecx()->decr()->jne()->loope()->end();
+//print_r($x);
+$x->set('ecx',3)->set('ldp',1)->decr()->jne()->loope()->end();
 
-    //print_r($x);
-    $x->set('ecx',3)->set('ldp',1)->decr()->jne()->loope()->end();
+$x->set('ecx',3)->set('ldp',2)->decr()->mov_ecx()->decr()->jne()->loop();
 
-    $x->set('ecx',3)->set('ldp',2)->decr()->mov_ecx()->decr()->jne()->loop();
+$x->set('ecx',3)->set('ldp',1)->decr()->mov_ecx()->decr()->jmp();
 
-    $x->set('ecx',3)->set('ldp',1)->decr()->mov_ecx()->decr()->jmp();
+$x->decr()->decr()->jgz()->decr()->set('ldp',7)->loopnz();
 
-    $x->decr()->decr()->jgz()->decr()->set('ldp',7)->loopnz();
+$x->set('ecx',3)->set('ldp',1)->decr()->mov_ecx()->decr()->jmp()->loop();
 
-    $x->set('ecx',3)->set('ldp',1)->decr()->mov_ecx()->decr()->jmp()->loop();
-
-    print_r($x);
+print_r($x);
